b76e55c0b7445ac9f7aab3f196ba44c772022814
cmd <cmd@cmd.local>
2026-06-21 10:01:45 -0500

    chore: bump PWA service worker to v38 (2026-06-21-24)

--- public/sw.js
+++ public/sw.js
@@ -1,5 +1,5 @@
-const CACHE_NAME = 'salonmoney-shell-v37';
-const APP_VERSION = '2026-06-21-23';
+const CACHE_NAME = 'salonmoney-shell-v38';
+const APP_VERSION = '2026-06-21-24';
 const SHELL_ASSETS = ['/', '/manifest.json', '/icons/icon.svg?v=6'];
 
 self.addEventListener('install', (event) => {
