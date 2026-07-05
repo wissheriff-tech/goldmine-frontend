05809ba0282e5eca0c17c7033eb4905841ba33fe
cmd <cmd@cmd.local>
2026-07-05 18:47:20 -0500

    chore: anchor PWA to mygoldmine.dev, bump to v17

--- public/sw.js
+++ public/sw.js
@@ -1,5 +1,5 @@
-const CACHE_NAME = 'goldmine-shell-v16';
-const APP_VERSION = '2026-07-05-3';
+const CACHE_NAME = 'goldmine-shell-v17';
+const APP_VERSION = '2026-07-05-4';
 const SHELL_ASSETS = ['/', '/manifest.json', '/icons/icon.svg?v=6'];
 
 self.addEventListener('install', (event) => {
