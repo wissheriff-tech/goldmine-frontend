8710727d3bba4c3aeef22853bbc6d8c258a57a4a
cmd <cmd@cmd.local>
2026-07-05 16:27:17 -0500

    chore: bump PWA to v16 / 2026-07-05-3

--- public/sw.js
+++ public/sw.js
@@ -1,5 +1,5 @@
-const CACHE_NAME = 'goldmine-shell-v15';
-const APP_VERSION = '2026-07-05-2';
+const CACHE_NAME = 'goldmine-shell-v16';
+const APP_VERSION = '2026-07-05-3';
 const SHELL_ASSETS = ['/', '/manifest.json', '/icons/icon.svg?v=6'];
 
 self.addEventListener('install', (event) => {
